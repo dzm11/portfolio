3cb17ad0c4a913b7a09d0bd406a559e4f66fa870
dzm11 <piotr.brze95@gmail.com>
2022-12-02 17:59:18 +0100

    dodatkow astrona kiddy

--- app/js/script.js
+++ app/js/script.js
@@ -1,73 +1,68 @@
 // Device Friendly Product Slider With Progress Bar + circle mouse
 // https://www.youtube.com/watch?v=LPBGK1gqXoU
 
-// let el = document.querySelector('.container');
-// let st = window.getComputedStyle(el);
-// let tr = st.getPropertyValue("transform") ||
-//        st.getPropertyValue("-moz-transform") ||
-//        st.getPropertyValue("-ms-transform") ||
-//        st.getPropertyValue("-o-transform") ||
-//        st.getPropertyValue("transform") || FAIL;
-// // var values = tr.split('(')[1];
-
-
-
-const slider = document.querySelector('.software__grid'); //cursor too
-const progressBar = document.querySelector('.progress__bar');
-let mouseCursor = document.querySelector('.mouse__cursor')
-
-let sliderGrabbed = false;
-
-slider.parentElement.addEventListener('scroll', (e) => {
-    progressBar.style.width  = `${getScrollPercentage()}%`;
-
-})
-
-slider.addEventListener('mousedown', (e) => {
-    sliderGrabbed = true;
-})
-
-slider.addEventListener('mouseup', (e) => {
-    sliderGrabbed = false;
-})
-
-slider.addEventListener('mouseleave', (e) => {
-    sliderGrabbed = false;
-    mouseCursor.classList.remove('mouse__cursor--active');
-})
-
-slider.addEventListener('mouseenter', (e) => {
-  mouseCursor.classList.add('mouse__cursor--active');
-})
-
-slider.addEventListener('mousemove', (e) => {
-  let el = document.querySelector('.container');
-  let st = window.getComputedStyle(el);
-  let tr = st.getPropertyValue("-webkit-transform") ||
-            st.getPropertyValue("-moz-transform") ||
-            st.getPropertyValue("-ms-transform") ||
-            st.getPropertyValue("-o-transform") ||
-            st.getPropertyValue("transform") || FAIL;
-    var values = tr.split('(')[1];
-    values = values.split(')')[0];
-    values = values.split(', ');
-    // console.log(values[5]);
-
-    mouseCursor.style.top = ((+values[5] * (-1)) + +e.clientY) + 'px';
-    mouseCursor.style.left = e.clientX + 'px';
-
-    if(sliderGrabbed){
-        slider.parentElement.scrollLeft -= e.movementX;
-    }
-})
-
-// slider.addEventListener('wheel', (e) =>{
-//     e.preventDefault()
-//     slider.parentElement.scrollLeft += e.deltaY;
-// })
-
-function getScrollPercentage(){
-   return ((slider.parentElement.scrollLeft / (slider.parentElement.scrollWidth - slider.parentElement.clientWidth) ) * 100);
+try{
+  const slider = document.querySelector('.software__grid'); //cursor too
+  const progressBar = document.querySelector('.progress__bar');
+  let mouseCursor = document.querySelector('.mouse__cursor')
+  
+  let sliderGrabbed = false;
+  
+  slider.parentElement.addEventListener('scroll', (e) => {
+      progressBar.style.width  = `${getScrollPercentage()}%`;
+  
+  })
+  
+  slider.addEventListener('mousedown', (e) => {
+      sliderGrabbed = true;
+  })
+  
+  slider.addEventListener('mouseup', (e) => {
+      sliderGrabbed = false;
+  })
+  
+  slider.addEventListener('mouseleave', (e) => {
+      sliderGrabbed = false;
+      mouseCursor.classList.remove('mouse__cursor--active');
+  })
+  
+  slider.addEventListener('mouseenter', (e) => {
+    mouseCursor.classList.add('mouse__cursor--active');
+  })
+  
+  slider.addEventListener('mousemove', (e) => {
+    let el = document.querySelector('.container');
+    let st = window.getComputedStyle(el);
+    let tr = st.getPropertyValue("-webkit-transform") ||
+              st.getPropertyValue("-moz-transform") ||
+              st.getPropertyValue("-ms-transform") ||
+              st.getPropertyValue("-o-transform") ||
+              st.getPropertyValue("transform") || FAIL;
+      var values = tr.split('(')[1];
+      values = values.split(')')[0];
+      values = values.split(', ');
+      // console.log(values[5]);
+  
+      mouseCursor.style.top = ((+values[5] * (-1)) + +e.clientY) + 'px';
+      mouseCursor.style.left = e.clientX + 'px';
+  
+      if(sliderGrabbed){
+          slider.parentElement.scrollLeft -= e.movementX;
+      }
+  })
+  
+  // slider.addEventListener('wheel', (e) =>{
+  //     e.preventDefault()
+  //     slider.parentElement.scrollLeft += e.deltaY;
+  // })
+  
+  function getScrollPercentage(){
+     return ((slider.parentElement.scrollLeft / (slider.parentElement.scrollWidth - slider.parentElement.clientWidth) ) * 100);
+  }
+  
+}
+catch(err){
+console.log(err);
 }
 
 
