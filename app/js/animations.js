b6daeb54e1f7c28a2f30cb9c2f990f60b01cfe2d
dzm11 <piotr.brze95@gmail.com>
2022-12-21 19:45:10 +0100

    commenting barbaJS aniamtions

--- app/js/animations.js
+++ app/js/animations.js
@@ -5,24 +5,27 @@ const strText = text.textContent;
 const splitText = strText.split("");
 text.textContent = "";
 
+// changing every letter to span
 for (let i = 0; i < splitText.length; i++) {
 text.innerHTML += "<span>" + splitText[i] + "</span>"
 }
 
 let char = 0;
-let timer = setInterval(onTick, 50);
+let timer = setInterval(onTick, 50); // latency between letter aniamtion
 
+// Letter animations
 function onTick(){
   const span = text.querySelectorAll('span')[char]
   span.classList.add('fade');
   char++;
   if(char===splitText.length){
     complete();
-    preloader.classList.add('hide');
+    preloader.classList.add('hide'); //Adding class for hiding black box
     return;
   }
 }
 
+//Stopping the timer
 function complete(){
   clearInterval(timer);
   timer = null;
@@ -36,6 +39,7 @@ barba.init({
   debug: true,
   transitions: [
 {
+    // Slide from home to project
       name: 'cover',
       from:{
         namespace: ['home']
@@ -45,6 +49,7 @@ barba.init({
       },
       leave(){}
     },
+    // Slide on entering project page and leaving it
     {
       name: 'cover',
       from:{
